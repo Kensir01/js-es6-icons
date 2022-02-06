01d3c58b9cbd7412604dfa9075aca7667c1ac396
Demetrio Turchetta <Kensir96@gmail.com>
2022-02-06 12:29:43 +0100

    Push di fine esercizio (spero)

---
+++ main.js
@@ -0,0 +1,158 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'purple'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'purple'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'purple'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'purple'
+	}
+];
+
+const contenitore = document.getElementById('contenitore-icone');
+
+disegnaIcone(contenitore, icons);
+
+const selezionato = document.getElementById('filtroPerTipo');
+
+selezionato.addEventListener('change', function() {
+    
+    let selezione = this.value;
+
+    if (selezione == "") {
+        disegnaIcone(contenitore, icons);
+    } else {
+        const elementiFiltrati = icons.filter(icon => {
+
+        if(icon.type == selezione) {
+            return true;
+        }
+
+        return false;
+    });
+
+    disegnaIcone(contenitore, elementiFiltrati);
+
+    }
+
+});
+
+
+function disegnaIcone(contenitore, icons) {
+
+    let contenutoIcone = '';
+
+    icons.forEach(oggetto => {
+        contenutoIcone += `<div class="icon">
+                        <i style="color:${oggetto.color};" class="${oggetto.family} ${oggetto.prefix}${oggetto.name}"></i>
+                        <div class="icon-text">${oggetto.name}</div>
+                    </div>`;
+    });
+
+    contenitore.innerHTML = contenutoIcone;
+
+}
